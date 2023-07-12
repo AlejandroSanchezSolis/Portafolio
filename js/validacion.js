6dc3943b5419e9f4a2948a54ab6c90fcc8581e6b
Alejandro Sanchez <alessolis26@gmail.com>
2023-07-11 21:51:35 -0400

    correccion de formato y orden de archivos

---
+++ js/validacion.js
@@ -0,0 +1,58 @@
+const nombre = document.getElementById("nombre");
+nombre.addEventListener("blur", () => {
+    if (nombre.validity.patternMismatch || nombre.validity.tooLong) {
+        nombre.setCustomValidity("Debe contener máximo 50 caracteres y solo debe incluir letras");
+    } else {
+        nombre.setCustomValidity("");
+    }
+});
+
+const telefono = document.getElementById("telefono");
+telefono.addEventListener("input", () => {
+    if (telefono.validity.patternMismatch) {
+        telefono.setCustomValidity("Su teléfono debería tener entre 7-10 digitos y no debe contener espacios ni prefijos");
+    } else {
+        telefono.setCustomValidity("");
+    }
+});
+
+const email = document.getElementById("email");
+email.addEventListener("input", () => {
+    if (email.validity.patternMismatch) {
+        email.setCustomValidity("Su correo debe contener el caracter especial (@) seguido de un dominio o proveedor seguido de un punto(.)");
+    } else {
+        email.setCustomValidity("");
+    }
+});
+
+const asunto = document.getElementById("asunto");
+asunto.addEventListener("input", () => {
+    if (asunto.validity.patternMismatch) {
+        asunto.setCustomValidity("Debe contener un máximo de 50 caracteres");
+    } else {
+        asunto.setCustomValidity("");
+    }
+});
+
+const textarea = document.getElementById("textarea");
+textarea.addEventListener("input", () => {
+    if (textarea.validity.patternMismatch) {
+        textarea.setCustomValidity("Debe contener un máximo de 300 caracteres");
+    } else {
+        textarea.setCustomValidity("");
+    }
+});
+
+let areadetexto = document.getElementById("textarea");
+let contador = document.getElementById("contador");
+function actualizarContador() {
+    let caracteres = areadetexto.value.length;
+    contador.textContent = caracteres + "/" + "300";
+}
+textarea.addEventListener("input", actualizarContador);
+
+
+
+
+
+
