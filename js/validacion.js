1fd97e47b83e73e3863bf90ad736f172f0a553e1
Alejandro Sanchez <alessolis26@gmail.com>
2023-07-11 22:14:28 -0400

    arreglo contador

--- js/validacion.js
+++ js/validacion.js
@@ -43,6 +43,17 @@ textarea.addEventListener("input", () => {
     }
 });
 
+let areadetexto = document.getElementById("textarea");
+let contador = document.getElementById("contador");
+function actualizarContador() {
+    let caracteres = areadetexto.value.length;
+    contador.textContent = caracteres + "/" + "300";
+}
+textarea.addEventListener("input", actualizarContador);
+
+
+
+
 
 
 
